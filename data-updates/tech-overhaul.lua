f2c2b10410d16113f7162f8e6028e10eb2916197
Em Karisch <em@khaos.solutions>
2025-09-18 13:41:08 +0200

    feat: unlock research control combinator via circuit network tech (#3)

---
+++ data-updates/tech-overhaul.lua
@@ -0,0 +1,3 @@
+data.raw.technology["research-control-combinator"] = nil
+
+table.insert(data.raw.technology["circuit-network"].effects, {type = "unlock-recipe", recipe = "Research_Control_Combinator"})
