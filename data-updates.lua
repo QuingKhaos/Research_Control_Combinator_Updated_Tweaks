876c9612e5a5c4f8ba37d082ce0fe43078b102ff
Em Karisch <em@khaos.solutions>
2025-09-18 13:18:01 +0200

    feat: unlock research control combinator via circuit network tech

--- data-updates.lua
+++ data-updates.lua
@@ -3,3 +3,6 @@ require("data-updates.schall-circuit-group")
 
 -- Research control combinator should be unlocked by researching, not be available from the start
 require("data-updates.research-unlock")
+
+-- Move research control combinator recipe to circuit network tech, removes custom tech
+require("data-updates.tech-overhaul")
