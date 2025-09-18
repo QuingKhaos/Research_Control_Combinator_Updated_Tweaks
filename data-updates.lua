601ebfa6c5aa364f3a4bb9e8f9cc87496caec8b0
Em Karisch <em@khaos.solutions>
2025-09-18 13:03:58 +0200

    fix: disable research control combinator
    to be unlocked via research

--- data-updates.lua
+++ data-updates.lua
@@ -1,2 +1,5 @@
 -- Better subgroup placement with SchallCircuitGroup
 require("data-updates.schall-circuit-group")
+
+-- Research control combinator should be unlocked by researching, not be avaialable from the start
+require("data-updates.research-unlock")
